0635aa8b211429db8cfd0dcda143da625af5f5ff
OriHero <mebungler@gmail.com>
2020-04-07 17:40:26 +0500

    Fix

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,60 +1,73 @@
-import axios from 'axios';
+import axios from "axios";
 
 const tempToken =
-    '$2y$10$1DiytpaACWq4AqZ.Wlo6DOKkNy3X9TwwJ5mIyPk0kchPI6ARFGW2e';
+	"$2y$10$1DiytpaACWq4AqZ.Wlo6DOKkNy3X9TwwJ5mIyPk0kchPI6ARFGW2e";
 
-export const URL = 'https://api.avtogen.uz';
+export const URL = "https://api.avtogen.uz";
 
 export let configureAxios = storeInstance => {
-    axios.interceptors.request.use(req => {
-        req.headers = {
-            Authorization: `Bearer ${storeInstance.getState().user.token}`,
-        };
-        return req;
-    });
+	axios.interceptors.request.use(req => {
+		req.headers = {
+			Authorization: `Bearer ${storeInstance.getState().user.token}`
+		};
+		return req;
+	});
 };
 
-let formData = (rawData) => {
-    let form = new FormData();
-    Object.keys(rawData).forEach(key => {
-        form.append(key, rawData[key]);
-    });
-    return form;
+let formData = rawData => {
+	let form = new FormData();
+	Object.keys(rawData).forEach(key => {
+		if (Array.isArray(rawData[key])) {
+			rawData[key].forEach(element => {
+				form.append(`${key}[]`, element);
+			});
+			return;
+		}
+		form.append(key, rawData[key]);
+	});
+	return form;
 };
 
 let request = {
-    profile: {
-        showProfile: () => axios.get(`${URL}/profile/show`),
-        showCompany: () => axios.get(`${URL}/profile/showCompany`),
-        setFcmToken: (data: object) => axios.post(`${URL}/profile/setFcmToken`, formData(data))
-    },
-    auth: {
-        login: (credentials: object) =>
-            axios.post(`${URL}/auth/login`, formData(credentials)),
-        verifyCode: (id: number, credentials: object) =>
-            axios.put(`${URL}/auth/login-verify/${id}`, credentials),
-    },
-    user: {
-        updateUser: (credentials: any) =>
-            axios.post(`${URL}/profile/update`, formData(credentials)),
-        show: () => axios.get(`${URL}/profile/show`),
-    },
-    services: {
-        getAll: () => axios.get(`${URL}/hand/services`)
-    },
-    handbook: {
-        getCarTypes: () => axios.get(`${URL}/hand/car-types`)
-    },
-    booking: {
-        getAllOrders: (status: string) => axios.get(`${URL}/booking/agent-books?status=${status}`),
-        create: (data: object) => axios.post(`${URL}/booking/agent-book`, formData(data)),
-        accept: (id: number) => axios.get(`${URL}/booking/accept-book/${id}`),
-        reject: (id: number) => axios.get(`${URL}/booking/reject-book/${id}`),
-        setStatus: (id: number, status: any) => axios.post(`${URL}/booking/set-status-book`, formData({
-            booking_id: id,
-            status
-        }))
-    },
+	profile: {
+		showProfile: () => axios.get(`${URL}/profile/show`),
+		showCompany: () => axios.get(`${URL}/profile/showCompany`),
+		setFcmToken: (data: object) =>
+			axios.post(`${URL}/profile/setFcmToken`, formData(data))
+	},
+	auth: {
+		login: (credentials: object) =>
+			axios.post(`${URL}/auth/login`, formData(credentials)),
+		verifyCode: (id: number, credentials: object) =>
+			axios.put(`${URL}/auth/login-verify/${id}`, credentials)
+	},
+	user: {
+		updateUser: (credentials: any) =>
+			axios.post(`${URL}/profile/update`, formData(credentials)),
+		show: () => axios.get(`${URL}/profile/show`)
+	},
+	services: {
+		getAll: () => axios.get(`${URL}/hand/services`)
+	},
+	handbook: {
+		getCarTypes: () => axios.get(`${URL}/hand/car-types`)
+	},
+	booking: {
+		getAllOrders: (status: string) =>
+			axios.get(`${URL}/booking/agent-books?status=${status}`),
+		create: (data: object) =>
+			axios.post(`${URL}/booking/agent-book`, formData(data)),
+		accept: (id: number) => axios.get(`${URL}/booking/accept-book/${id}`),
+		reject: (id: number) => axios.get(`${URL}/booking/reject-book/${id}`),
+		setStatus: (id: number, status: any) =>
+			axios.post(
+				`${URL}/booking/set-status-book`,
+				formData({
+					booking_id: id,
+					status
+				})
+			)
+	}
 };
 
 export default request;
--- src/views/account/NewOrder.tsx
+++ src/views/account/NewOrder.tsx
@@ -1,95 +1,108 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Property, { PropertyProps } from '../../components/common/Property';
-import { colors } from '../../constants';
-import RoundButton from '../../components/common/RoundButton';
-import strings from '../../locales/strings';
-import Counter from '../../components/common/Counter';
-import { properties } from '../../components/OrderPill'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Property, { PropertyProps } from "../../components/common/Property";
+import { colors } from "../../constants";
+import RoundButton from "../../components/common/RoundButton";
+import strings from "../../locales/strings";
+import Counter from "../../components/common/Counter";
+import { properties } from "../../components/OrderPill";
 
 interface NewOrderProps {
-  properties: PropertyProps[];
-  accept: Function;
-  decline: Function;
+	properties: PropertyProps[];
+	accept: Function;
+	decline: Function;
 }
 
 const NewOrder = ({ accept, decline, ...item }: NewOrderProps) => {
-  console.warn(item.time);
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.orderText}>{strings.order}</Text>
-        <Counter />
-      </View>
-      <Property
-        title={properties[0].title}
-        description={typeof item.time === 'string' && item.time.split(" ")[0]}
-        rightText={typeof item.time === 'string' && item.time.split(" ")[1]}
-      />
-      <Property
-        title={properties[1].title}
-        icon={
-          !!item && item.car_type && item.car_type.icon
-            ? item.car_type.icon
-            : properties[1].icon
-        }
-        description={
-          item.car_type
-            ? item.car_type.title
-            : properties[1].description
-        }
-      />
-      <Property title={strings.typeOfService} description={item.booking_services && item.booking_services.reduce((prev, current) => { return prev + current.service.title + '\n\n' }, "")} />
-      <Property
-        title={properties[3].title}
-        price={
-          !!item && item.total_cost ? item.total_cost : properties[3].price
-        }
-      />
-      <View style={styles.row}>
-        <RoundButton
-          text={strings.decline}
-          full
-          flex
-          textColor={colors.darkGray}
-          borderColor={colors.extraGray}
-          backgroundColor={colors.extraGray}
-          onPress={() => decline(item)}
-        />
-        <RoundButton
-          text={strings.details}
-          fill
-          full
-          flex
-          onPress={accept}
-          backgroundColor={colors.yellow}
-        />
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={styles.orderText}>{strings.order}</Text>
+				<Counter />
+			</View>
+			<Property
+				title={properties[0].title}
+				description={
+					typeof item.time === "string" && item.time.split(" ")[0]
+				}
+				rightText={
+					typeof item.time === "string" && item.time.split(" ")[1]
+				}
+			/>
+			<Property
+				title={properties[1].title}
+				icon={
+					!!item && item.car_type && item.car_type.icon
+						? item.car_type.icon
+						: properties[1].icon
+				}
+				description={
+					item.car_type
+						? item.car_type.title
+						: properties[1].description
+				}
+			/>
+			<Property
+				title={strings.typeOfService}
+				description={
+					item.booking_services &&
+					item.booking_services.reduce((prev, current) => {
+						return prev + current.service.title + "\n\n";
+					}, "")
+				}
+			/>
+			<Property
+				title={properties[3].title}
+				price={
+					!!item && item.total_cost
+						? item.total_cost
+						: properties[3].price
+				}
+			/>
+			<View style={styles.row}>
+				<RoundButton
+					text={strings.decline}
+					full
+					flex
+					textColor={colors.darkGray}
+					borderColor={colors.extraGray}
+					backgroundColor={colors.extraGray}
+					onPress={() => decline(item)}
+				/>
+				<RoundButton
+					text={strings.details}
+					fill
+					full
+					flex
+					onPress={accept}
+					backgroundColor={colors.yellow}
+				/>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  header: {
-    paddingVertical: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  orderText: {
-    color: colors.accent,
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  counter: {},
-  container: {
-    borderRadius: 25,
-    backgroundColor: colors.white,
-    padding: 15,
-  },
-  row: {
-    paddingTop: 20,
-    flexDirection: 'row',
-  },
+	header: {
+		paddingVertical: 20,
+		flexDirection: "row",
+		justifyContent: "space-between"
+	},
+	orderText: {
+		color: colors.accent,
+		fontSize: 18,
+		fontWeight: "bold"
+	},
+	counter: {},
+	container: {
+		borderRadius: 25,
+		backgroundColor: colors.white,
+		padding: 15
+	},
+	row: {
+		paddingTop: 20,
+		flexDirection: "row"
+	}
 });
 
 export default NewOrder;
