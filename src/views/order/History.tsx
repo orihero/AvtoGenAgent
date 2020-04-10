9e1c7d5653f578371b861a1afebbbe8aa9f01670
OriHero <mebungler@gmail.com>
2020-04-11 03:42:32 +0500

    Removed time periods from history

--- src/views/order/History.tsx
+++ src/views/order/History.tsx
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
 import RoundButton, {
-  RoundButtonProps,
-} from '../../components/common/RoundButton';
-import { colors } from '../../constants';
-import strings from '../../locales/strings';
-import OrderItem from './OrderItem';
-import { demoOrder } from '../account/Account';
-import OrderPill from '../../components/OrderPill';
-import request from '../../api/requests';
+	RoundButtonProps
+} from "../../components/common/RoundButton";
+import { colors } from "../../constants";
+import strings from "../../locales/strings";
+import OrderItem from "./OrderItem";
+import { demoOrder } from "../account/Account";
+import OrderPill from "../../components/OrderPill";
+import request from "../../api/requests";
 
 let activeButton: RoundButtonProps = {
-  backgroundColor: colors.yellow,
-  text: colors.black,
-  borderColor: colors.transparent,
+	backgroundColor: colors.yellow,
+	text: colors.black,
+	borderColor: colors.transparent
 };
 let inActiveButton: RoundButtonProps = {
-  borderColor: colors.lightGray,
-  textColor: colors.darkGray,
+	borderColor: colors.lightGray,
+	textColor: colors.darkGray
 };
 
 const History = () => {
-  const [activeIndex, setactiveIndex] = useState(0);
-  const [data, setData] = useState([]);
-  let effect = async () => {
-    let res = await request.booking.getAllOrders('done');
-    setData(res.data.data)
-  }
-  useEffect(() => {
-    effect()
-  }, [])
-  return (
-    <View style={styles.container}>
-      <View style={styles.row}>
-        <RoundButton
+	const [activeIndex, setactiveIndex] = useState(0);
+	const [data, setData] = useState([]);
+	let effect = async () => {
+		let res = await request.booking.getAllOrders("done");
+		setData(res.data.data);
+	};
+	useEffect(() => {
+		effect();
+	}, []);
+	return (
+		<View style={styles.container}>
+			<View style={styles.row}>
+				{/* <RoundButton
           full
           flex
           borderColor={colors.lightGray}
@@ -46,26 +46,28 @@ const History = () => {
           flex
           backgroundColor={colors.yellow}
           text={strings.inToday}
-        />
-      </View>
-      <FlatList
-        keyExtractor={(e, i) => i.toString()}
-        renderItem={({ item, ...props }) => <OrderPill {...props} {...{ item }} />}
-        data={data}
-      />
-    </View>
-  );
+        /> */}
+			</View>
+			<FlatList
+				keyExtractor={(e, i) => i.toString()}
+				renderItem={({ item, ...props }) => (
+					<OrderPill {...props} {...{ item }} />
+				)}
+				data={data}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 15,
-    flex: 1,
-    backgroundColor: colors.ultraLightGray,
-  },
-  row: {
-    flexDirection: 'row',
-  },
+	container: {
+		padding: 15,
+		flex: 1,
+		backgroundColor: colors.ultraLightGray
+	},
+	row: {
+		flexDirection: "row"
+	}
 });
 
 export { History };
